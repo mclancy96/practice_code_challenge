33cad995420cff6aa2fd1d42a7c7d19c95240bb8
Mike <mclancy96@gmail.com>
2025-07-29 08:15:46 -0400

    fix line requiring lib dir

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,3 @@ ActiveRecord::Base.establish_connection({
                                           adapter: 'sqlite3',
                                           database: 'db/development.db'
                                         })
-
-require_all './lib'
