202a771a4efe2b60aaf99db066700962aa5f3ebd
Mike <mclancy96@gmail.com>
2025-07-29 11:04:27 -0400

    Basic methods for author complete

--- app/models/author.rb
+++ app/models/author.rb
@@ -29,4 +29,31 @@ class Author < ActiveRecord::Base
   def self.alphabetical
     Author.all.order(name: :asc)
   end
+
+  def first_book
+    books.order(published_year: :asc).first
+  end
+
+  def last_book
+    books.order(published_year: :asc).last
+  end
+
+  def self.with_no_books
+    Author.all.select { |author| author.book_count.zero? }
+  end
+
+  def self.with_books_in_genre(genre)
+    return unless Genre.find_by(name: genre)
+
+    Genre.find_by(name: genre).books.map(&:author).uniq
+  end
+
+  def has_written_in_genre?(genre)
+    authors_in_genre = self.class.with_books_in_genre(genre)
+    authors_in_genre ? authors_in_genre.include?(self) : false
+  end
+
+  def all_book_titles
+    books.map(&:title)
+  end
 end
